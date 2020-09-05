254edb2acb6d34b0d0b0bdf8ddc03f2ceabb0b7d
Timofey Tavlintsev <timmy88@gmail.com>
2020-09-05 18:55:00 +0300

    Tim's kyria keymap up

--- keyboards/kyria/keymaps/techtim/keymap.c
+++ keyboards/kyria/keymaps/techtim/keymap.c
@@ -33,21 +33,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Base Layer: QWERTY
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  | RCTL   |
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |                              |   Y  |   U  |   I  |   O  |   P  | BSPC   |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * | LCTL   |   A  |   S  |  D   |   F  |   G  |                              |   H  |   J  |   K  |   L  | ;  : |  ' "   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
- * | LSFT   |   Z  |   X  |   C  |   V  |   B  | Bspc | Raise|  |Adjust| Esc  |   N  |   M  | ,  < | . >  | /  ? | LSFT   |
+ * | LSFT   |   Z  |   X  |   C  |   V  |   B  | Bspc | Raise|  |Adjust| Esc  |   N  |   M  | ,  < | . >  | /  ? | Lower  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Enc   | LALT |LGUI  |Space | Lower|  | Enter| Space| [ {  | ] }  |      |
+ *                        |Enc   | LALT |LGUI  |Space | Lower|  | Enter| Space| [ {  | ] }  |Raise |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-      KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_RCTL,
-      KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,    KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-      KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B, KC_BSPC, MO(_RAISE), MO(_ADJUST), KC_ESC,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
-                         ENC_MODE_L,KC_LALT, KC_LGUI, KC_SPC, MO(_LOWER),    KC_ENT,     KC_SPC,     KC_LBRC,  KC_RBRC,  MO(_RAISE)
+        KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,                                                KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        KC_LCTL, KC_A,   KC_S,   KC_D,   KC_F,    KC_G,                                                KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,    KC_B, KC_BSPC, MO(_RAISE), MO(_ADJUST), KC_ESC,     KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH, MO(_LOWER),
+                         ENC_MODE_L,  KC_LALT, KC_LGUI,  KC_SPC, MO(_LOWER),    KC_ENT,     KC_SPC,     KC_LBRC,  KC_RBRC,  MO(_RAISE)
     ),
 /*
  * Lower Layer: Symbols, Navigation
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Raise Layer: Functions, Numbers
  *
  * ,-------------------------------------------.                              ,-------------------------------------------.
- * |        |      | F7   | F8   | F9   |      |                              |      | 7 &  | 8 *  | 9 (  | - _  |        |
+ * |        |      | F7   | F8   | F9   |      |                              |      | 7 &  | 8 *  | 9 (  | - _  |   *    |
  * |--------+------+------+------+------+------|                              |------+------+------+------+------+--------|
  * |        |      | F4   | F5   | F6   |      |                              |      | 4 *  | 5 %  | 6 ^  | = +  | RCTL   |
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
