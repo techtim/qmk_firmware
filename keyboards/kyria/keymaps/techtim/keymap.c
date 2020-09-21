4308e2988d5019d78abfb391bf6b19dd013b4605
Tim Tavlinsev <ttavlintsev@enttec.com>
2020-09-21 18:47:59 +0300

    Space<->win

--- keyboards/kyria/keymaps/techtim/keymap.c
+++ keyboards/kyria/keymaps/techtim/keymap.c
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------+-------------.  ,-------------+------+------+------+------+------+--------|
  * | LSFT   |   Z  |   X  |   C  |   V  |   B  | Bspc | Raise|  |Adjust| Esc  |   N  |   M  | ,  < | . >  | /  ? | Lower  |
  * `----------------------+------+------+------+------+------|  |------+------+------+------+------+----------------------'
- *                        |Enc   | LALT |LGUI  |Space | Lower|  | Enter| Space| [ {  | ] }  |Raise |
+ *                        |Enc   | LALT |Space | LGUI | Lower|  | Enter| Space| [ {  | ] }  |Raise |
  *                        |      |      |      |      |      |  |      |      |      |      |      |
  *                        `----------------------------------'  `----------------------------------'
  */
@@ -47,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MACRO_CTL_ESC,        KC_Q,   KC_W,   KC_E,   KC_R,    KC_T,                                           KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
         MT(MOD_LCTL, KC_TAB), KC_A,   KC_S,   KC_D,   KC_F,    KC_G,                                           KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT,              KC_Z,   KC_X,   KC_C,   KC_V,    KC_B, KC_BSPC, MO(_RAISE), MO(_ADJUST), KC_ESC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MO(_LOWER),
-                                      ENC_MODE_L,  KC_LALT, KC_LGUI, KC_SPC,  MO(_LOWER), KC_ENT, KC_SPC, KC_LBRC, KC_RBRC,  MO(_RAISE)
+                                      ENC_MODE_L,  KC_LALT,  KC_SPC, KC_LGUI,  MO(_LOWER), KC_ENT, KC_SPC, KC_LBRC, KC_RBRC,  MO(_RAISE)
     ),
 /*
  * Lower Layer: Symbols, Navigation
